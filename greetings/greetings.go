1c35c4477bbc2a5fbb0109ce880a02ef8736d9b3
bhupendra <bhupendrap@sostronk.com>
2023-05-04 15:22:58 +0530

    Add error handle for empty name and logger

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Greetings(name string) string {
+func Greetings(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("gimme a name dumbass")
+	}
 
 	greetMSg := fmt.Sprintf("hello %v. How are you doing", name)
 
-	return greetMSg
+	return greetMSg, nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Bhupendrapt5/go-mod-demo/greetings"
 )
 
 func main() {
-	message := greetings.Greetings("Gladys")
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Greetings("")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	fmt.Println(message)
 }
